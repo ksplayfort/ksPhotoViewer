7462bbe53b43aaaa8a031b6dd40fa91b3e5766cd
ksplayfort <265175647+ksplayfort@users.noreply.github.com>
2026-05-14 12:02:35 +0700

    Add files via upload

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -1,6 +1,6 @@
 /*
  * KSPhotoViewer
- * Copyright (C) 2024 KS
+ * Copyright (C) 2026 KS
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
